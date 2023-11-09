c79b4eff5845fc32be67eb7d056e966f52562cb2
Sall <sallaboudaouda@gmail.com>
2023-11-09 08:56:56 +0000

    feat(i18n): added i18n config

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,14 @@
-export default async function Home() {
+import { getDictionary, locale } from '@/i18n'
+
+type Props = {
+    params: {
+        lang: locale
+    }
+}
+
+export default async function Home({ params: { lang } }: Props) {
+    const intl = await getDictionary(lang)
+    console.log(intl, 'intl')
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             this is the home page
