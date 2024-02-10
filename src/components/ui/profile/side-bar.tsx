9b6c6f70ef397c46a5a39c735da6e1ee27cb4481
Sall <sallaboudaouda@gmail.com>
2024-02-10 18:27:22 +0000

    feat(sidebar): added sidebar

--- src/components/ui/profile/side-bar.tsx
+++ src/components/ui/profile/side-bar.tsx
@@ -2,6 +2,8 @@
 import React, { FC } from 'react'
 import { HelpCircleIcon, History, Pencil } from 'lucide-react'
 import { useSidebar } from '../../../../context'
+import Link from 'next/link'
+import Image from 'next/image'
 
 interface Link {
     Icon: JSX.Element
@@ -10,15 +12,15 @@ interface Link {
 
 const links: Link[] = [
     {
-        Icon: <Pencil size={20} />,
+        Icon: <Pencil size={17} />,
         name: 'Edit profile'
     },
     {
-        Icon: <History size={20} />,
+        Icon: <History size={17} />,
         name: 'History'
     },
     {
-        Icon: <HelpCircleIcon size={20} />,
+        Icon: <HelpCircleIcon size={17} />,
         name: 'Help'
     }
 ]
@@ -27,12 +29,15 @@ export const SideBar: FC = () => {
     const { setClickedLink, clickedLink } = useSidebar()
 
     return (
-        <div className="w-52 h-screen fixed pl-16">
+        <div className="w-52 h-screen fixed pl-16 bg-slate-200 pt-4 top-0">
+            <Link href="/">
+                <Image src="/images/Logo.png" width={34} height={34} alt="logo" priority />
+            </Link>
             <div className="mt-8">
                 <ul className="flex gap-9 flex-col">
                     {links.map(({ name, Icon }, key) => (
                         <p
-                            className="flex gap-4 items-center"
+                            className="flex gap-4 items-center hover:cursor-pointer text-sm text-primary-200 hover:underline"
                             key={key}
                             onClick={() => setClickedLink(name)}>
                             <span>{Icon}</span>
