907bbc8dcb443376c29afd62757891422e987205
Sall <sallaboudaouda@gmail.com>
2024-03-14 11:00:00 +0000

    feat(paymentform): added stripe payment form

--- src/components/ui/total/total.tsx
+++ src/components/ui/total/total.tsx
@@ -3,20 +3,33 @@ import { useState } from 'react'
 import clsx from 'clsx'
 import { useSelector } from 'react-redux'
 import { BiDollar } from 'react-icons/bi'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
-import { Button } from '..'
+import { Button, PaymentForm } from '..'
 import { selectIngredients } from '@/redux/selectors/ingredients'
 import { totalFormatter } from '@/utils/utils'
 import { Modal } from '../modal/modal'
 import { ShippingAddress } from '../shipping-address'
+import { useAppSelector } from '@/redux/hook'
+import { selectShippingAddress } from '@/redux/selectors/shipping-address'
+
+const stripePromise = loadStripe(`${process.env.NEXT_STRIPE_PUBLIC_KEY}`)
 
 export const Total = () => {
     const { ingredients, totalPrice } = useSelector(selectIngredients)
+    let [isPaymentOpen, setIsPaymeOpen] = useState(false)
+    const { shippingAddress } = useAppSelector(selectShippingAddress)
     const { meat, salad, bacon, cheese } = ingredients
     let [isOpen, setIsOpen] = useState(false)
 
     const handleOrder = () => {
         setIsOpen(true)
+    }
+
+    const handlePayment = () => {
+        setIsOpen(false)
+        setIsPaymeOpen(true)
     }
 
     return (
@@ -64,7 +77,17 @@ export const Total = () => {
                 isOpen={isOpen}
                 setIsOpen={setIsOpen}
                 title="Add your shipping address"
-                content={<ShippingAddress />}
+                content={<ShippingAddress handlePayment={handlePayment} />}
+            />
+            <Modal
+                isOpen={isPaymentOpen}
+                setIsOpen={setIsPaymeOpen}
+                title="Payment"
+                content={
+                    <Elements stripe={stripePromise}>
+                        <PaymentForm />
+                    </Elements>
+                }
             />
         </div>
     )
