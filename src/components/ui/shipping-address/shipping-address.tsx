7209644c5aeeca1275180d4d01bf8040996a9d2d
Sall <sallaboudaouda@gmail.com>
2024-09-12 20:14:58 +0000

    refactor(shipping-address): refactored shipping-address

--- src/components/ui/shipping-address/shipping-address.tsx
+++ src/components/ui/shipping-address/shipping-address.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
@@ -19,6 +20,7 @@ type ShippingFormValues = {
     streetAddress: string
     town: string
 }
+
 interface ShippingAddressProps {
     handlePayment: () => void
     isOpen: boolean
@@ -27,7 +29,7 @@ interface ShippingAddressProps {
 export function ShippingAddress({ handlePayment, isOpen }: ShippingAddressProps) {
     const dispatch = useDispatch()
     const { shippingAddress } = useAppSelector(selectShippingAddress)
-    const session = useSession()
+    const { data: session } = useSession()
     const isShippingAddressPresent = Object.values(shippingAddress).every((value) => value !== '')
     const {
         handleSubmit,
@@ -42,68 +44,66 @@ export function ShippingAddress({ handlePayment, isOpen }: ShippingAddressProps)
         dispatch(addShippingAddress(data))
     }
 
-    return (
-        <div>
-            {session ? (
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex gap-8">
-                        <Input
-                            name="firstName"
-                            type="text"
-                            placeholder="First name"
-                            register={register}
-                            errors={errors}
-                        />
-                        <Input
-                            name="lastName"
-                            type="text"
-                            placeholder="Last name"
-                            register={register}
-                            errors={errors}
-                        />
-                    </div>
-                    <Input
-                        name="streetAddress"
-                        type="text"
-                        placeholder="Street address"
-                        register={register}
-                        errors={errors}
-                    />
-                    <Input
-                        name="town"
-                        type="text"
-                        placeholder="Town/City"
-                        register={register}
-                        errors={errors}
-                    />
+    if (!session) {
+        return (
+            <div className="p-4 rounded-md">
+                <p className="flex flex-col gap-4">
+                    <span className="text-primary-200 text-sm">
+                        You should be logged in to make an order.
+                    </span>
+                    <Link href="/auth/sign-in" className="text-sm underline">
+                        Go to login page
+                    </Link>
+                </p>
+            </div>
+        )
+    }
 
-                    {isShippingAddressPresent ? (
-                        <Button
-                            type="submit"
-                            label="Proceed to payment"
-                            className="w-full h-10"
-                            onClick={handlePayment}
-                        />
-                    ) : (
-                        <Button
-                            type="submit"
-                            label="Add shipping address"
-                            className="w-full h-10"
-                        />
-                    )}
-                </form>
-            ) : (
-                <div className="p-4 rounded-md">
-                    <p className="flex flex-col gap-4">
-                        <span className="text-primary-200 text-sm">
-                            You should be logged in to make an order.
-                        </span>
-                        <Link href="/auth/sign-in" className="text-sm  underline">
-                            Go to login page
-                        </Link>
-                    </p>
+    return (
+        <div className="shipping-address">
+            <form onSubmit={handleSubmit(onSubmit)} aria-live="polite">
+                <div className="flex gap-8">
+                    <Input
+                        name="firstName"
+                        type="text"
+                        placeholder="First name"
+                        register={register}
+                        errors={errors}
+                        aria-invalid={!!errors.firstName}
+                    />
+                    <Input
+                        name="lastName"
+                        type="text"
+                        placeholder="Last name"
+                        register={register}
+                        errors={errors}
+                        aria-invalid={!!errors.lastName}
+                    />
                 </div>
-            )}
+                <Input
+                    name="streetAddress"
+                    type="text"
+                    placeholder="Street address"
+                    register={register}
+                    errors={errors}
+                    aria-invalid={!!errors.streetAddress}
+                />
+                <Input
+                    name="town"
+                    type="text"
+                    placeholder="Town/City"
+                    register={register}
+                    errors={errors}
+                    aria-invalid={!!errors.town}
+                />
+
+                <Button
+                    type="submit"
+                    label={isShippingAddressPresent ? 'Proceed to payment' : 'Add shipping address'}
+                    className="w-full h-10"
+                    onClick={isShippingAddressPresent ? handlePayment : undefined}
+                />
+            </form>
         </div>
     )
 }
