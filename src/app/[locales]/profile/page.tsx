447036c7c72b14271f0283875b6979d12b263644
Sall <sallaboudaouda@gmail.com>
2024-09-15 16:42:59 +0000

    feat(content): added content on profile

--- src/app/[locales]/profile/page.tsx
+++ src/app/[locales]/profile/page.tsx
@@ -1,15 +1,11 @@
 import { Metadata } from 'next'
 
-import { UpdateProfileForm } from '@/components/ui'
+import Content from './content'
 
 export const metadata: Metadata = {
     title: 'Profile',
     description: 'Manage your account'
 }
 export default function Profile() {
-    return (
-        <section className="px-16 mt-16">
-            <UpdateProfileForm />
-        </section>
-    )
+    return <Content />
 }
