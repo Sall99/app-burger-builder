22eca9a83cef117d67c72fc12e4678973966e83d
Sall <sallaboudaouda@gmail.com>
2024-09-18 12:33:32 +0000

    feat(locales): added locales on help

--- src/components/ui/help/help.tsx
+++ src/components/ui/help/help.tsx
@@ -1,45 +1,39 @@
 'use client'
+
 import React from 'react'
 import { Disclosure } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
-
-const helpData = [
-    {
-        title: 'How to Build Your Burger?',
-        description: `
-            To build your burger, start by selecting your desired ingredients from the list provided. Choose from a variety of options including fresh vegetables like lettuce, tomatoes, and onions, as well as cheeses, sauces, and premium toppings like avocado or bacon. Customize your burger to your liking by adding or removing ingredients as you go.
-        `
-    },
-    {
-        title: 'Customizing Your Burger',
-        description: `
-            Take customization to the next level by selecting the type of bun, from classic options like sesame or brioche to gluten-free alternatives. Adjust the number of patties and layer on additional toppings or sauces to create your perfect burger. Whether you prefer a single, double, or even triple patty, the choice is yours!
-        `
-    },
-    {
-        title: 'Reviewing Your Order',
-        description: `
-            Before placing your order, take a moment to review your burger creation. Ensure all selected ingredients are correct and make any necessary adjustments, such as changing quantities or removing items. Our review screen provides a clear summary of your choices to help you confirm your perfect burger before checkout.
-        `
-    },
-    {
-        title: 'Placing Your Order',
-        description: `
-            Once your burger is just right, proceed by clicking the "Place Order" button. You will be guided through our simple and secure checkout process, where you can enter your delivery details, choose a payment method, and finalize your order. Your burger will be on its way in no time!
-        `
-    },
-    {
-        title: 'Special Offers',
-        description: `
-            Don’t miss out on our special offers! Visit the promotions section to discover current discounts and exclusive deals available for your order. Whether it's a limited-time discount or a combo deal, there's always an opportunity to save on your next burger.
-        `
-    }
-]
+import { useTranslations } from 'next-intl'
 
 export default function Help() {
+    const t = useTranslations('Help')
+
+    const helpData = [
+        {
+            title: t('HowToBuild'),
+            description: t('HowToBuildDescription')
+        },
+        {
+            title: t('Customizing'),
+            description: t('CustomizingDescription')
+        },
+        {
+            title: t('Reviewing'),
+            description: t('ReviewingDescription')
+        },
+        {
+            title: t('PlacingOrder'),
+            description: t('PlacingOrderDescription')
+        },
+        {
+            title: t('SpecialOffers'),
+            description: t('SpecialOffersDescription')
+        }
+    ]
+
     return (
         <div className="px-4 md:px-8 lg:px-12 mx-auto">
-            <h2 className="text-2xl font-bold mb-8 text-zinc-700">Help Center</h2>
+            <h2 className="text-2xl font-bold mb-8 text-zinc-700">{t('HelpTitle')}</h2>
             <div className="bg-white shadow-md rounded-lg p-4">
                 {helpData.map(({ title, description }, index) => (
                     <Disclosure key={index} as="div" className="mt-4" defaultOpen={index === 0}>
