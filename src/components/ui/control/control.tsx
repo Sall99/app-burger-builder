62f7e14c3b6bba02bb0e11ec81e25385e0e95d3e
Sall <sallaboudaouda@gmail.com>
2024-09-18 12:16:36 +0000

    feat(locales): add locales on control

--- src/components/ui/control/control.tsx
+++ src/components/ui/control/control.tsx
@@ -1,38 +1,40 @@
-'use client';
-import { AiOutlineMinus } from 'react-icons/ai';
-import { MdAdd } from 'react-icons/md';
-import { useDispatch } from 'react-redux';
+'use client'
 
-import { addIngredients, removeIngredients } from '@/redux/slices/ingredients';
+import { AiOutlineMinus } from 'react-icons/ai'
+import { MdAdd } from 'react-icons/md'
+import { useDispatch } from 'react-redux'
+import { useTranslations } from 'next-intl'
+
+import { addIngredients, removeIngredients } from '@/redux/slices/ingredients'
 
 export const Controls = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
+    const t = useTranslations('Ingredients')
+
     const controls = [
         { label: 'Meat', type: 'meat' },
         { label: 'Bacon', type: 'bacon' },
         { label: 'Cheese', type: 'cheese' },
         { label: 'Salad', type: 'salad' }
-    ];
+    ]
 
     return (
         <div className="build-controls">
             <div className="controls">
-                {controls.map((ctrl) => {
-                    return (
-                        <div key={ctrl.label}>
-                            <p className="label">{ctrl.label}</p>
-                            <div className="ctrl">
-                                <div onClick={() => dispatch(addIngredients(ctrl.type))}>
-                                    <MdAdd />
-                                </div>
-                                <div onClick={() => dispatch(removeIngredients(ctrl.type))}>
-                                    <AiOutlineMinus />
-                                </div>
+                {controls.map((ctrl) => (
+                    <div key={ctrl.label}>
+                        <p className="label">{t(`${ctrl.label}`)}</p>
+                        <div className="ctrl">
+                            <div onClick={() => dispatch(addIngredients(ctrl.type))}>
+                                <MdAdd />
+                            </div>
+                            <div onClick={() => dispatch(removeIngredients(ctrl.type))}>
+                                <AiOutlineMinus />
                             </div>
                         </div>
-                    );
-                })}
+                    </div>
+                ))}
             </div>
         </div>
-    );
-};
+    )
+}
