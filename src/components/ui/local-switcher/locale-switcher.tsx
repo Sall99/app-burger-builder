f807759475d2d7a288f96139c326f0936a74ee02
Sall <sallaboudaouda@gmail.com>
2024-09-19 10:58:00 +0000

    feat(localswitcher): fix local swither

--- src/components/ui/local-switcher/locale-switcher.tsx
+++ src/components/ui/local-switcher/locale-switcher.tsx
@@ -1,21 +1,20 @@
 'use client'
 
-import React, { startTransition, useState, useTransition } from 'react'
-import { useParams, usePathname, useRouter } from 'next/navigation'
+import React, { useState, useTransition } from 'react'
+import { useRouter } from 'next/navigation'
 import { useLocale } from 'next-intl'
 
 const locales = [{ name: 'en' }, { name: 'fr' }]
 
 const LocaleSwitcher = () => {
     const router = useRouter()
-    const [selected, setSelected] = useState(locales[0])
-    const pathName = usePathname()
-    const currentLocale = useParams()
     const [isPending, startTransition] = useTransition()
-
     const localActive = useLocale()
+    const [selected, setSelected] = useState(localActive || locales[0].name)
 
-    const handleLocaleChange = (e: { target: { value: any } }) => {
+    const handleLocaleChange = (e: { target: { value: string } }) => {
+        const selectedLocale = e.target.value
+        setSelected(selectedLocale)
         startTransition(() => {
             router.replace(`/${e.target.value}`)
         })
@@ -26,7 +25,7 @@ const LocaleSwitcher = () => {
             <select
                 onChange={handleLocaleChange}
                 className="bg-slate-200 rounded-md h-10 px-2 text-[#f08e4a] text-sm"
-                value={currentLocale.lang}>
+                value={selected}>
                 {locales.map((locale) => (
                     <option key={locale.name} value={locale.name}>
                         {locale.name}
