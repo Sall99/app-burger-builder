5a00f8200acbf09dae4181ee3406abe5676177a1
Sall <sallaboudaouda@gmail.com>
2024-09-25 13:05:10 +0000

    refactor: fix dynamic usage

--- src/app/api/orders/get-orders/route.ts
+++ src/app/api/orders/get-orders/route.ts
@@ -1,30 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import prisma from '../../../../../libs/prisma.db'
+import { prisma } from '../../../../../libs/prisma.db'
 import { getUserAndSession } from '../../../../../libs/session'
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export const dynamic = 'force-dynamic'
+
+export async function GET(req: NextRequest) {
     try {
         const { user } = await getUserAndSession()
 
-        const userId = user.id
-
         const orders = await prisma.order.findMany({
             where: {
-                userId
+                userId: user.id
             },
             include: {
                 shippingAdresse: true
             }
         })
 
-        return NextResponse.json(
-            {
-                orders
-            },
-            { status: 200 }
-        )
+        return NextResponse.json({ orders }, { status: 200 })
     } catch (error) {
-        return NextResponse.json({ error })
+        console.error('Error fetching orders:', error)
+        if (error instanceof Error && error.message === 'Unauthorized') {
+            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+        }
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
